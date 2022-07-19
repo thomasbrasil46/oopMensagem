4f13a2bde2e444be231f1f18f11ba2911c0e769f
thomasbrasil46 <83769834+thomasbrasil46@users.noreply.github.com>
2022-07-19 09:50:17 -0300

    AddingLostFiles

---
+++ Mensagem.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooMensagem
+{
+    public class Mensagem
+    {
+        //private String TextoMensagem;
+
+        //public void ExibirMensagem()
+        //{
+        //    Console.WriteLine(this.TextoMensagem);
+        //}
+
+        //public String getTextoMensagem()
+        //{
+        //    return this.TextoMensagem;
+        //}
+
+        //public void setTextoMensagem(String valor)
+        //{
+        //    this.TextoMensagem = valor.ToUpper();
+        //}
+
+        private String textoMensagem;
+
+        public String TextoMensagem
+        {
+            get
+            {
+                return this.textoMensagem;
+            }
+
+            set
+            {
+                this.textoMensagem = value.ToUpper();
+            }
+        }
+
+        public void ExibirMensagem()
+        {
+            Console.WriteLine(this.TextoMensagem); 
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooMensagem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mensagem msg1, msg2;
+            msg1 = new Mensagem();
+            msg1.TextoMensagem = "Hello, World!";
+            msg1.ExibirMensagem(); 
+
+            /*msg2 = new Mensagem();
+            //msg2.TextoMensagem = "Segundo objeto";
+            msg2.ExibirMensagem();*/
+
+            Console.ReadKey();
+        }
+    }
+}
